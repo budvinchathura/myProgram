47f08de400424e90150f25cf583ca067327e9146
Budvin Chathura <bc@example.com>
2018-12-28 17:09:03 +0530

    change2

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 public static void main(String[] args) {
-	Person person1 = new Person("Nimal",22);
+	Person person1 = new Person("John",22);
 	person1.sayName();
+	Person person2 = new Person("Doe")
 }
