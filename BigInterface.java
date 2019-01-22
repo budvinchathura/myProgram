95f9b4b52badf5974abbacde2ef72ba0c7dae5b0
Budvin Chathura <bc@example.com>
2019-01-22 19:37:53 +0530

    another method added

--- BigInterface.java
+++ BigInterface.java
@@ -1,19 +1,21 @@
 package hw1;
 
 public interface BigInterface {
-	
+
 	int x=0;
 	static int y=1;
 	static final int z=7;
-	
+
 	public default void Method1() {
 		int a=8;
 		int b=a*7;
 	}
-	
+
 	public void Method2();
-	
-	
-	
+
+	public void anotherMethod();
+
+
+
 
 }
